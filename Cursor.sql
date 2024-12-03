101c465b0bb221c569538ae65ce8a835e06851e3
AbhijithNaiju <abhijithnaiju@gmail.com>
2024-12-03 18:25:26 +0530

    Commit 03-12

---
+++ Cursor.sql
@@ -0,0 +1,21 @@
+DECLARE @name VARCHAR
+DECLARE @age INT
+
+DECLARE listCursor CURSOR FOR 
+	SELECT name,age from userAge
+
+OPEN listCursor 
+
+FETCH NEXT FROM listCursor into @name,@age
+
+WHILE(@@FETCH_STATUS = 0)
+BEGIN
+	
+	PRINT 'Name : ' + @name + ' Age : ' + cast(@age as varchar(20))
+
+	FETCH NEXT FROM listCursor INTO @name,@age
+
+END 
+
+CLOSE listCursor
+DEALLOCATE listCursor
---
+++ trigger.sql
@@ -0,0 +1,9 @@
+CREATE OR ALTER TRIGGER dbo.printTables
+ON userAge
+AFTER INSERT
+AS
+BEGIN
+	SELECT * FROM userAge
+END
+
+INSERT INTO userAge VALUES('Jithin',25) 
